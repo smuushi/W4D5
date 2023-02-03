201e547115450f01dd13ce320b1460e792c38b74
andy <morrison.andrew422@gmail.com>
2023-02-03 15:08:49 -0500

    blcs

--- bigO/big_o.rb
+++ bigO/big_o.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 # O(n^2) - due to nested each loop
 def my_min(list_arr)
   min_ele = list_arr[0]
@@ -11,8 +13,8 @@ def my_min(list_arr)
   min_ele
 end
 
-list_arr = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-puts my_min(list_arr) # => -5
+# list_arr = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# puts my_min(list_arr) # => -5
 
 
 # O(n) - due to 1 conditional call per ele
@@ -26,12 +28,10 @@ def my_min_opt(list_arr)
   min_ele
 end
 
-puts my_min_opt(list_arr) # => -5
+# puts my_min_opt(list_arr) # => -5
 
 
 def largest_contiguous_subsum(list)
-
-  out_arr = []
 
   sub_arrays = []
   list.each_with_index do |ele1, idx1|
@@ -52,9 +52,66 @@ def largest_contiguous_subsum(list)
   return max_sum
 
 end
+# O(n^2) + n => evaluates to n^2 becuase it will dominate the method in a large input
 
 # list = [5, 3, -7]
 # p largest_contiguous_subsum(list)
 
+# list = [2, 3, -6, 7, -6, 7]
+# p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+def better_largest_contiguous_subsum(list)
+  
+  current_sum = list.first
+  largest_sum = list.first
+
+  # count = 0
+
+  # (1...list.length).each do |idx|
+  #   # current_sum = list[idx]
+  #   if current_sum + list[idx] > current_sum
+  #     current_sum = current_sum + list[idx]
+  #   else
+  #     largest_sum = current_sum
+  #     current_sum = list[idx + 1]
+  #   end
+  # end
+
+  list.each_with_index do |ele, idx|
+    # debugger
+    
+    if idx == 0
+      current_sum = ele
+      next
+    end
+
+    if (current_sum + ele) > current_sum
+      current_sum = current_sum + ele
+    else
+      largest_sum = current_sum
+      current_sum = ele
+    end
+    
+  end
+  
+  # j = i
+  # while i < list.length
+  #   current_sum = list[i..j].sum
+  #   if current_sum > max_sum
+  #     max_sum = current_sum
+  #   end
+  #   if j < list.length
+  #     j += 1
+  #   else
+  #     i += 1
+  #   end
+  #   count += 1
+  # end
+
+  return largest_sum
+
+end
+# list = [2, 2]
+
 list = [2, 3, -6, 7, -6, 7]
-p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+p better_largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
