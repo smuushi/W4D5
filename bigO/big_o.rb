5d437d940fc53c7918ba80686f5da5de945f8e2e
smuushi <mishih780@gmail.com>
2023-02-03 14:07:09 -0500

    doing well

--- bigO/big_o.rb
+++ bigO/big_o.rb
@@ -27,3 +27,34 @@ def my_min_opt(list_arr)
 end
 
 puts my_min_opt(list_arr) # => -5
+
+
+def largest_contiguous_subsum(list)
+
+  out_arr = []
+
+  sub_arrays = []
+  list.each_with_index do |ele1, idx1|
+
+    list.each_with_index do |ele2, idx2|
+      sub_arrays << list[idx1..idx2] if idx1 <= idx2
+    end
+
+  end
+  
+  max_sum = sub_arrays.first.sum
+
+  sub_arrays.each do |sub_arr|
+    max_sum = sub_arr.sum if sub_arr.sum > max_sum
+
+  end
+
+  return max_sum
+
+end
+
+# list = [5, 3, -7]
+# p largest_contiguous_subsum(list)
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
